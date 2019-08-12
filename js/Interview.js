d51c59cbdb6e8e76e5ad0bbef6a025811acdbf9a
lizhiyuan <lizhiyuan@calfdata.com>
2019-08-12 14:14:43 +0800

    cdn

--- js/Interview.js
+++ js/Interview.js
@@ -27,6 +27,7 @@ export class Interview {
             })
         })
     }
+
     //防抖
     static debounce(fn, time) {
         let timer = null;
@@ -40,6 +41,7 @@ export class Interview {
             }, time);
         }
     }
+
     //节流
     static throttle(fn, gapTime) {
         let lastTime = null;
@@ -51,18 +53,19 @@ export class Interview {
             }
         }
     }
+
     //闭包
-    static closure(){
+    static closure() {
         function f() {
-            let i=0;
+            let i = 0;
             return function () {
                 console.log(++i);
             }
         }
 
-        var b=[];
-        for(var i=0;i<10;i++){
-            b[i]=(function (j) {
+        var b = [];
+        for (var i = 0; i < 10; i++) {
+            b[i] = (function (j) {
                 return function () {
                     return j
                 }
@@ -70,14 +73,73 @@ export class Interview {
         }
 
         function moduleA() {
-            let name='aaa';
+            let name = 'aaa';
+
             function sayName() {
                 console.log(name)
             }
+
             return {
                 sayName
             }
         }
+
         moduleA().sayName();
+    }
+
+    //实现flat函数(两种)
+    static flat(src, dst = []) {
+        src.forEach(v => {
+            if (Array.isArray(v)) {
+                Interview.flat(v, dst);
+            } else {
+                dst.push(v);
+            }
+        });
+        return dst
+    }
+
+    static flatten(arr) {
+        return arr.reduce((flat, toFlatten) => {
+            return flat.concat(Array.isArray(toFlatten) ? Interview.flatten(toFlatten) : toFlatten);
+        }, []);
+    }
+
+    //函数去重(四种)
+    static arrayUniq(arr) {
+        let obj = {};
+        let res = [];
+        if (arr.length > 1) {
+            arr.forEach(v => {
+                if (!obj[v]) {
+                    res.push(v);
+                    obj[v] = true;
+                }
+            })
+        } else {
+            res=arr;
+        }
+        return res;
+    }
+    static es6Uniq(arr){
+        return [...new Set(arr)];
+    }
+    static mapUniq(arry) {
+        let map = new Map();
+        let result = new Array();
+        for (let i = 0; i < arry.length; i++) {
+            if (map.has(arry[i])) {
+                map.set(arry[i], true);
+            } else {
+                map.set(arry[i], false);
+                result.push(arry[i]);
+            }
+        }
+        return result;
+    }
+    static reduceUniq(arr){
+        return arr.reduce((pre,cur)=>{
+            return pre.includes(cur)?pre:[...pre,cur]
+        },[])
     }
 }
