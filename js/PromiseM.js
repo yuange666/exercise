7ad753f1e264f860641476eec47932de2890aa25
lizhiyuan <lizhiyuan@calfdata.com>
2019-08-12 17:30:47 +0800

    promise

---
+++ js/PromiseM.js
@@ -0,0 +1,6 @@
+//定义Promise的三种状态
+const PENDING='PENDING';
+const FULFILLED ='FULFILLED';
+const REJECTED='REJECTED';
+// 判断变量否为function
+const isFunction = variable => typeof variable === 'function';
